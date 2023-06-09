f3d09574db2e969c499c7c8c3f8602eae6ebcde4
RationalDysaniaer <122082502+RationalDysaniaer@users.noreply.github.com>
2023-06-09 16:08:21 +0800

    Update Explode.java

--- Explode.java
+++ Explode.java
@@ -2,10 +2,10 @@ import java.awt.*;
 
 public class Explode extends GameObject{
     static  Image[] picture = new Image[16];
-    int explodeCount = 0;
+    int explodeCount = 0;//爆炸图片计数
     static{
         for(int i = 0;i<picture.length;i++){
-            picture[i] = Toolkit.getDefaultToolkit().getImage("C:\\Users\\ASUS\\javaworkspace\\untitled\\AircraftBattle\\src\\Images\\e"+(i+1)+".gif");
+            picture[i] = Toolkit.getDefaultToolkit().getImage("src\\Images\\e"+(i+1)+".gif");
         }
     }
     public Explode(int x, int y) {
@@ -14,14 +14,13 @@ public class Explode extends GameObject{
 
     @Override
     public void paintSelf(Graphics gImage) {
-        super.paintSelf(gImage);
-        if (explodeCount < 16){
+        // 每一次绘制不会消失，下一次repaint方法再次调用绘制下一张图片，直到爆炸图片绘制完
+        if (explodeCount <= 15){
             img = picture[explodeCount];
             super.paintSelf(gImage);
             explodeCount++;
         }
-        // 每一次绘制不会消失，下一次repaint方法再次调用重绘，知道爆炸图片绘制完
-        if(explodeCount == 16) {
+        else {
             GameUtils.explodeList.remove(this);
         }
     }
